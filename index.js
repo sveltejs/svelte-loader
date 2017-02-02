a10117fa66f5792ec2254fec7ae9948e985965ff
Joe Coulson <coulson84@hotmail.com>
2017-02-02 21:55:40 +0000

    adding a "generate" option that can be passed and will default to "dom"

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = function(source, map) {
       shared: query.shared || false,
       name: query.name,
       css: query.css !== false,
+      generate: query.generate || 'dom',
     });
 
     this.callback(null, code, map);
