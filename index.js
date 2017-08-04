e637a8e85a16f3fd8f9c46c8f2662f7744194fbe
Rich Harris <richard.a.harris@gmail.com>
2017-08-04 13:38:45 -0400

    format with prettier

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ const { compile } = require('svelte');
 const { getOptions } = require('loader-utils');
 
 function sanitize(input) {
-  return basename(input)
-    .replace(extname(input), '')
-    .replace(/[^a-zA-Z_$0-9]+/g, '_')
-    .replace(/^_/, '')
-    .replace(/_$/, '')
-    .replace(/^(\d)/, '_$1');
+	return basename(input)
+		.replace(extname(input), '')
+		.replace(/[^a-zA-Z_$0-9]+/g, '_')
+		.replace(/^_/, '')
+		.replace(/_$/, '')
+		.replace(/^(\d)/, '_$1');
 }
 
 function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1);
+	return str[0].toUpperCase() + str.slice(1);
 }
 
 module.exports = function(source, map) {
-  this.cacheable();
+	this.cacheable();
 
-  const options = getOptions(this) || {};
+	const options = getOptions(this) || {};
 
-  options.filename = this.resourcePath;
-  options.format = this.version === 1 ? options.format || 'cjs' : 'es';
-  options.shared = options.format === 'es' && require.resolve( 'svelte/shared.js' );
+	options.filename = this.resourcePath;
+	options.format = this.version === 1 ? options.format || 'cjs' : 'es';
+	options.shared =
+		options.format === 'es' && require.resolve('svelte/shared.js');
 
-  if (!options.name) options.name = capitalize(sanitize(options.filename));
+	if (!options.name) options.name = capitalize(sanitize(options.filename));
 
-  try {
-    let { code, map } = compile(source, options);
+	try {
+		let { code, map } = compile(source, options);
 
-    this.callback(null, code, map);
-  } catch (err) {
-    // wrap error to provide correct
-    // context when logging to console
-    this.callback(new Error(err.toString() + '\n' + err.frame));
-  }
+		this.callback(null, code, map);
+	} catch (err) {
+		// wrap error to provide correct
+		// context when logging to console
+		this.callback(new Error(err.toString() + '\n' + err.frame));
+	}
 };
--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,219 +1,229 @@
 /* global describe, it */
-
 const chai = require('chai');
-var sinonChai = require('sinon-chai');
-chai.use(sinonChai);
-
-const expect = chai.expect;
-
+const sinonChai = require('sinon-chai');
 const { spy } = require('sinon');
-
 const { readFileSync } = require('fs');
-
 const loader = require('../');
 
+chai.use(sinonChai);
+const { expect } = chai;
 
-describe('loader', function() {
+describe('loader', () => {
+	function testLoader(fileName, callback, query, version = 2) {
+		return () => {
+			const fileContents = readFile(fileName);
+			const cacheableSpy = spy(() => {});
+			const callbackSpy = spy(callback);
 
-  function testLoader(fileName, callback, query, version = 2) {
+			loader.call(
+				{
+					cacheable: cacheableSpy,
+					callback: callbackSpy,
+					resourcePath: fileName,
+					version,
+					query
+				},
+				fileContents,
+				null
+			);
 
-    return function() {
+			expect(callbackSpy).to.have.been.called;
+			expect(cacheableSpy).to.have.been.called;
+		};
+	}
 
-      const fileContents = readFile(fileName);
+	it(
+		'should compile',
+		testLoader('test/fixtures/good.html', function(err, code, map) {
+			expect(err).not.to.exist;
+			expect(code).to.exist;
+			expect(map).to.exist;
+		})
+	);
 
-      const cacheableSpy = spy(function() { });
+	describe('error handling', () => {
+		it(
+			'should handle parse error',
+			testLoader('test/fixtures/parse-error.html', function(
+				err,
+				code,
+				map,
+				context
+			) {
+				expect(err).to.exist;
 
-      const callbackSpy = spy(callback);
+				expect(err.message).to.eql(
+					'ParseError: Expected }}}\n' +
+						'1: <p>Count: {{{count}}</p>\n' +
+						'                     ^\n' +
+						"2: <button on:click='set({ count: count + 1 })'>+1</button>"
+				);
 
-      loader.call({
-        cacheable: cacheableSpy,
-        callback: callbackSpy,
-        resourcePath: fileName,
-        version,
-        query,
-      }, fileContents, null);
+				expect(code).not.to.exist;
+				expect(map).not.to.exist;
+			})
+		);
 
-      expect(callbackSpy).to.have.been.called;
-      expect(cacheableSpy).to.have.been.called;
-    };
-  }
+		it(
+			'should handle wrong export',
+			testLoader('test/fixtures/export-error.html', function(
+				err,
+				code,
+				map,
+				context
+			) {
+				expect(err).to.exist;
 
+				expect(err.message).to.eql(
+					'ParseError: Unexpected token\n' +
+						'3: <script>\n' +
+						'4:   export {\n' +
+						"5:     foo: 'BAR'\n" +
+						'          ^\n' +
+						'6:   };\n' +
+						'7: </script>'
+				);
 
-  it('should compile',
-    testLoader('test/fixtures/good.html', function(err, code, map) {
-      expect(err).not.to.exist;
+				expect(code).not.to.exist;
+				expect(map).not.to.exist;
+			})
+		);
 
-      expect(code).to.exist;
-      expect(map).to.exist;
-    })
-  );
+		it(
+			'should validation error',
+			testLoader('test/fixtures/validation-error.html', function(
+				err,
+				code,
+				map,
+				context
+			) {
+				expect(err).to.exist;
 
+				expect(err.message).to.eql(
+					'ValidationError: Computed properties can be function expressions or arrow function expressions\n' +
+						'4:   export default {\n' +
+						'5:     computed: {\n' +
+						"6:       foo: 'BAR'\n" +
+						'              ^\n' +
+						'7:     }\n' +
+						'8:   };'
+				);
 
-  describe('error handling', function() {
+				expect(code).not.to.exist;
+				expect(map).not.to.exist;
+			})
+		);
+	});
 
-    it('should handle parse error',
-      testLoader('test/fixtures/parse-error.html', function(err, code, map, context) {
+	describe('ES2015 features', () => {
+		it(
+			'should keep imports / methods',
+			testLoader('test/fixtures/es2015.html', function(err, code, map) {
+				expect(err).not.to.exist;
 
-        expect(err).to.exist;
+				expect(code).to.exist;
+				expect(map).to.exist;
 
-        expect(err.message).to.eql(
-          'ParseError: Expected }}}\n' +
-          '1: <p>Count: {{{count}}</p>\n' +
-          '                     ^\n' +
-          '2: <button on:click=\'set({ count: count + 1 })\'>+1</button>'
-        );
+				// es2015 statements remain
+				expect(code).to.contain("import { hello } from './utils';");
+				expect(code).to.contain('data() {');
+			})
+		);
 
-        expect(code).not.to.exist;
-        expect(map).not.to.exist;
-      })
-    );
+		it(
+			'should keep nested Component import',
+			testLoader('test/fixtures/parent.html', function(err, code, map) {
+				expect(err).not.to.exist;
 
+				// es2015 statements remain
+				expect(code).to.contain("import Nested from './nested';");
 
-    it('should handle wrong export',
-      testLoader('test/fixtures/export-error.html', function(err, code, map, context) {
+				expect(code).to.exist;
+				expect(map).to.exist;
+			})
+		);
+	});
 
-        expect(err).to.exist;
+	describe('configuration via query', () => {
+		describe('css', () => {
+			it(
+				'should configure css (default)',
+				testLoader('test/fixtures/css.html', function(err, code, map) {
+					expect(err).not.to.exist;
+					expect(code).to.contain('function add_css ()');
+				})
+			);
 
-        expect(err.message).to.eql(
-          'ParseError: Unexpected token\n' +
-          '3: <script>\n' +
-          '4:   export {\n' +
-          '5:     foo: \'BAR\'\n' +
-          '          ^\n' +
-          '6:   };\n' +
-          '7: </script>'
-        );
+			it(
+				'should configure no css',
+				testLoader(
+					'test/fixtures/css.html',
+					function(err, code, map) {
+						expect(err).not.to.exist;
+						expect(code).not.to.contain('function add_css ()');
+					},
+					{ css: false }
+				)
+			);
+		});
 
-        expect(code).not.to.exist;
-        expect(map).not.to.exist;
-      })
-    );
+		describe('shared', () => {
+			it(
+				'should configure shared=false (default)',
+				testLoader(
+					'test/fixtures/good.html',
+					function(err, code, map) {
+						expect(err).not.to.exist;
 
+						expect(code).not.to.contain('import {');
+						expect(code).not.to.contain('svelte/shared.js');
+					},
+					{},
+					1
+				)
+			);
 
-    it('should validation error',
-      testLoader('test/fixtures/validation-error.html', function(err, code, map, context) {
+			it(
+				'should configure shared=true',
+				testLoader(
+					'test/fixtures/good.html',
+					function(err, code, map) {
+						expect(err).not.to.exist;
 
-        expect(err).to.exist;
+						expect(code).to.contain('import {');
+						expect(code).to.contain('svelte/shared.js');
+					},
+					{ shared: true }
+				)
+			);
+		});
 
-        expect(err.message).to.eql(
-          'ValidationError: Computed properties can be function expressions or arrow function expressions\n' +
-          '4:   export default {\n' +
-          '5:     computed: {\n' +
-          '6:       foo: \'BAR\'\n' +
-          '              ^\n' +
-          '7:     }\n' +
-          '8:   };'
-        );
+		describe('generate', () => {
+			it(
+				'should configure generate=undefined (default)',
+				testLoader('test/fixtures/good.html', function(err, code, map) {
+					expect(err).not.to.exist;
 
-        expect(code).not.to.exist;
-        expect(map).not.to.exist;
-      })
-    );
+					expect(code).not.to.contain('.render = function ( root, options ) {');
+				})
+			);
 
-  });
+			it(
+				'should configure generate=ssr',
+				testLoader(
+					'test/fixtures/good.html',
+					function(err, code, map) {
+						expect(err).not.to.exist;
 
-
-  describe('ES2015 features', function() {
-
-    it('should keep imports / methods',
-      testLoader('test/fixtures/es2015.html', function(err, code, map) {
-        expect(err).not.to.exist;
-
-        expect(code).to.exist;
-        expect(map).to.exist;
-
-        // es2015 statements remain
-        expect(code).to.contain('import { hello } from \'./utils\';');
-        expect(code).to.contain('data() {');
-      })
-    );
-
-
-    it('should keep nested Component import',
-      testLoader('test/fixtures/parent.html', function(err, code, map) {
-        expect(err).not.to.exist;
-
-        // es2015 statements remain
-        expect(code).to.contain('import Nested from \'./nested\';');
-
-        expect(code).to.exist;
-        expect(map).to.exist;
-      })
-    );
-
-  });
-
-
-  describe('configuration via query', function() {
-
-    describe('css', function() {
-
-      it('should configure css (default)',
-        testLoader('test/fixtures/css.html', function(err, code, map) {
-          expect(err).not.to.exist;
-          expect(code).to.contain('function add_css ()');
-        })
-      );
-
-
-      it('should configure no css',
-        testLoader('test/fixtures/css.html', function(err, code, map) {
-          expect(err).not.to.exist;
-          expect(code).not.to.contain('function add_css ()');
-        }, { css: false })
-      );
-
-    });
-
-    describe('shared', function() {
-
-      it('should configure shared=false (default)',
-        testLoader('test/fixtures/good.html', function(err, code, map) {
-          expect(err).not.to.exist;
-
-          expect(code).not.to.contain('import {');
-          expect(code).not.to.contain('svelte/shared.js');
-        }, {}, 1)
-      );
-
-
-      it('should configure shared=true',
-        testLoader('test/fixtures/good.html', function(err, code, map) {
-          expect(err).not.to.exist;
-
-          expect(code).to.contain('import {');
-          expect(code).to.contain('svelte/shared.js');
-        }, { shared: true })
-      );
-
-    });
-
-    describe('generate', function() {
-
-      it('should configure generate=undefined (default)',
-        testLoader('test/fixtures/good.html', function(err, code, map) {
-          expect(err).not.to.exist;
-
-          expect(code).not.to.contain('.render = function ( root, options ) {');
-        })
-      );
-
-
-      it('should configure generate=ssr',
-        testLoader('test/fixtures/good.html', function(err, code, map) {
-          expect(err).not.to.exist;
-
-          expect(code).to.contain('.render = function ( state, options ) {');
-        }, { generate: 'ssr' })
-      );
-
-    });
-
-  });
-
+						expect(code).to.contain('.render = function ( state, options ) {');
+					},
+					{ generate: 'ssr' }
+				)
+			);
+		});
+	});
 });
 
-
 function readFile(path) {
-  return readFileSync(path, 'utf-8');
+	return readFileSync(path, 'utf-8');
 }
